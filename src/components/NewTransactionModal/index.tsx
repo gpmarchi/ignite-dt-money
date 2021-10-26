706830c74e3e0fb88de4dfef1c020e41346ee5e3
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2021-10-26 18:07:33 -0300

    feat: Update state with new transaction

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -21,10 +21,10 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
 	const [category, setCategory] = useState('');
 	const [type, setType] = useState('deposit');
 
-	function handleCreateNewTransaction(event: FormEvent) {
+	async function handleCreateNewTransaction(event: FormEvent) {
 		event.preventDefault();
 
-		createTransaction({
+		await createTransaction({
 			title,
 			value,
 			category,
